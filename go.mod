83f057d657c306c4298554917cf260843781bb2f
fgorkhs <fgorkhs@gmail.com>
2021-05-12 18:55:17 +0800

    Got rid of junk files

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/akamensky/argparse v1.2.2 // indirect
 	github.com/c9s/goprocinfo v0.0.0-20210130143923-c95fcf8c64a8 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
--- heartbeat.go
+++ heartbeat.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/akamensky/argparse"
 	humanize "github.com/dustin/go-humanize"
 	memory_usage "github.com/mackerelio/go-osstat/memory"
 	disk_usage "github.com/ricochet2200/go-disk-usage/du"
@@ -18,11 +19,19 @@ func usage_stats(print_to_term bool) []byte {
 	cpu_use, _ := cpu.Percent(time.Second, false)
 
 	if print_to_term {
-		fmt.Printf("HD Free: %s\n", humanize.Bytes(hd_usage.Free()))
-		fmt.Printf("HD Used: %s\n", humanize.Bytes(hd_usage.Used()))
-		fmt.Printf("RAM used: %s\n", humanize.Bytes(ram_usage.Used))
-		fmt.Printf("RAM free: %s\n", humanize.Bytes(ram_usage.Free))
-		fmt.Printf("CPU pct: %.1f\n", cpu_use[0])
+		fmt.Printf(`
+		HD Free:  %s
+		HD Used:  %s
+		RAM used: %s
+		RAM free: %s
+		CPU:      %.1f %%
+
+`, 
+		humanize.Bytes(hd_usage.Free()),
+		humanize.Bytes(hd_usage.Used()),
+		humanize.Bytes(ram_usage.Used),
+		humanize.Bytes(ram_usage.Free),
+		cpu_use[0])
 	}
 
 	output_map := map[string]int{
@@ -32,19 +41,29 @@ func usage_stats(print_to_term bool) []byte {
 		"RAM_used_mb": int(ram_usage.Used / 1024 / 1024),
 		"RAM_free_mb": int(ram_usage.Free / 1024 / 1024),
 		"CPU_pct":     int(cpu_use[0])}
-
 	output_str, _ := json.Marshal(output_map)
 	return output_str
 }
 
 func main() {
-	jline := usage_stats(false)
+	// Argument parser
+	parser := argparse.NewParser("heartbeat", 
+								 "Just a simple system monitoring tool")
+	// Returns a pointer
+	// Should output print to terminal?
+	print_to_term := parser.Flag("c", "console", 
+						  &argparse.Options{Help: "Prints stats to console"})
+	parser.Parse(os.Args)
+
+	// Gathers usage stats
+	json_line := usage_stats(*print_to_term)
 	t := time.Now()
 	log_file_name := fmt.Sprintf("%d-%02d-%d_system_usage.log",
 		t.Year(), t.Month(), t.Day())
 
+	// Appends file, makes if doesn't exist
 	f, _ := os.OpenFile(log_file_name,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer f.Close()
-	f.WriteString(string(jline) + "\n")
+	f.WriteString(string(json_line) + "\n")
 }
